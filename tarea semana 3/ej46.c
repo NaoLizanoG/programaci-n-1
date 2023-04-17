cab8baff0183bdeff46db43d2dd8ba5d6a4c45e0
NaoLizanoG <naomilizano@gmail.com>
2023-04-17 18:51:02 -0500

    final con comentarios

--- tarea semana 3/ej46.c
+++ tarea semana 3/ej46.c
@@ -4,12 +4,16 @@
 #include<stdio.h>
 
 int main() {
+    //variables
     int i, a=0;
     printf("La tabla de multiplicar del 5 es:\n");
     
+    //ciclo for para los resultados de la mutiplicacion por cinco
     for (i=0; i<=60; i=i+5){
         
+        //mensaje que el usuario ve
         printf("5x%d=%d\n",a, i);
+        //a es los numeros a multiplicar por cinco para obtener los resultados de la tabla
         a=a+1;
     }
 
