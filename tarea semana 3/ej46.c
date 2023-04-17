dba2c749ac5a321f10e11734c78bae4817e187e3
NaoLizanoG <naomilizano@gmail.com>
2023-04-17 17:23:39 -0500

    version 1

---
+++ tarea semana 3/ej46.c
@@ -0,0 +1,16 @@
+//Naomi Lizano
+//36. Mostrar en pantalla la tabla de multiplicar del número 5.
+
+#include<stdio.h>
+
+int main() {
+    int i, b, a=5;
+
+for (i=1; i=12; i++){
+ 
+printf("La tabla de multiplicar del 5 es:\n");
+b=a*i;
+printf("5 x %d=%d\n", i, b );
+}
+return 0;
+}
