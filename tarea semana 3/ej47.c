b9e9e73b5d0ceb5a5654b6d57288ecc799974da3
NaoLizanoG <naomilizano@gmail.com>
2023-04-17 18:46:29 -0500

    final

--- tarea semana 3/ej47.c
+++ tarea semana 3/ej47.c
@@ -3,12 +3,18 @@
 #include<stdio.h>
 
 int main(){
+
+  //se declaran variables
 int i, a, r;
 
+//ciclo for para determinar las tablas del 1 al 10
 for(i=1;i<=10;i++){
     printf("La tabla del %d es:\n", i);
+    //ciclo for para determinar numeros a multiplicar del 0 al 12
     for(a=0;a<=12;a++){
+      //multiplicacion
       r=i*a;
+      //mensaje que se muestra al usuario
       printf("%dx%d=%d\n", i, a, r);
 
     }
