85559f35e2bee6cbe299b172232992d2208bac18
NaoLizanoG <naomilizano@gmail.com>
2023-04-18 19:47:46 -0500

    primera solucion

--- ej48.c
+++ ej48.c
@@ -1,2 +1,16 @@
 //Naomi Lizano
 //38. Leer un número entero y mostrar en pantalla su tabla de multiplicar.
+ 
+ #include<stdio.h>
+
+
+ int main(){
+int i, n, resultado;
+    for (i=0; i<=12; i++){
+        resultado=i*n;
+        printf("%dx%d=%d", )
+
+    }
+
+ }
+ 
