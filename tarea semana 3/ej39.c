c9a13ea217692a1718ce19bfaa472b3afa12035f
NaoLizanoG <naomilizano@gmail.com>
2023-04-19 17:23:08 -0500

    version 1

--- tarea semana 3/ej39.c
+++ tarea semana 3/ej39.c
@@ -1,2 +1,26 @@
 //Naomi Lizano
 //39.Utilizando el concepto de ciclo, generar la serie de Fibonacci hasta llegar o sobrepasar el número 10000.
+#include<stdio.h>
+#include<math.h>
+#include<conio.h>
+#include<locale.h>
+
+int main(){
+setlocale(LC_ALL,"");
+ int  nf1, nf2, nf, contador;
+
+  nf1=0;
+  printf("%i\n",nf1);
+
+  nf2=1;
+  printf("%i\n",nf2);
+
+   for(contador=3; contador<=20;contador++){
+    nf= nf1 + nf2;
+    printf("%i\n",nf);
+    nf1 = nf2;
+    nf2 = nf;
+  }
+getch();
+return 0;
+ }
