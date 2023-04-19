6ad6acccb708bc4802ef4bcd76dde041fd42ea20
NaoLizanoG <naomilizano@gmail.com>
2023-04-19 17:38:27 -0500

    proceso

--- tarea semana 3/ej40.c
+++ tarea semana 3/ej40.c
@@ -1,2 +1,25 @@
 //Naomi Lizano 
 //40. Leer un número de dos dígitos y determinar si pertenece a la serie de Fibonacci.
+#include<stdio.h>
+
+int num(int n){
+ int nf2=1, nf1=0, contador, resultado;
+ resultado =0;
+  
+    for(contador=3; contador<=22;contador++){
+    nf= nf1 + nf2;
+    if (n=nf){
+        resultado = resultado +1;
+    }
+    nf1 = nf2;
+    nf2 = nf;
+    }
+    return resultado;
+}
+
+int main(){
+
+printf("Escriba que numero desea evaluar \n");
+scanf()
+
+}
